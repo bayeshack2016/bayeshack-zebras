6033ac0adf0e33e03019de53488c2d87d44104ef
Vincent Van Steenbergen <v.vansteenbergen@gmail.com>
2016-04-24 00:07:08 +0100

    Add sql script

---
+++ sql/db.sql
@@ -0,0 +1,13 @@
+﻿DROP TABLE opioid_data;
+CREATE TABLE opioid_data
+(
+prescription_count INTEGER,
+overdose_count INTEGER,
+population INTEGER,
+year INTEGER,
+zip INTEGER,
+county INTEGER,
+state INTEGER,
+lat FLOAT,
+lon FLOAT
+);
