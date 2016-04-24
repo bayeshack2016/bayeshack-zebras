a9015f4067411dec37ae196e04c7c5f9122f6bba
Vincent Van Steenbergen <v.vansteenbergen@gmail.com>
2016-04-24 14:51:17 +0100

    Add python script to merge geojson

---
+++ merge_counties.py
@@ -0,0 +1,23 @@
+import json
+from pprint import pprint
+
+def combine_dicts(a, b):
+    dict = {}
+    dict.update(a)
+    dict.update(b)
+    return dict
+
+# Parse the geojson
+with open('data/us-counties.json') as geojson_file:
+    geojson = json.load(geojson_file)
+
+# Parse the counties data
+with open('data/counties_json.csv') as data_file:
+    for line in data_file:
+        data = json.loads(line)
+        properties = [combine_dicts(values['properties'],data) for values in geojson['features'] if values['properties']['STATE'] == data['state_id'] and values['properties']['COUNTY'] == data['county_id'] ]
+        pprint(properties)
+
+# Merge back counties data into geojson
+
+# Export as json
