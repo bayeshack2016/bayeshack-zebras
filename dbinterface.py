331b034fd9bf63c56a146ef5239904a96a161716
Henry Tominaga <htominaga@gmail.com>
2016-04-24 03:11:23 -0700

    pull entire db contents to client via ajax call and populate dynatable

---
+++ dbinterface.py
@@ -0,0 +1,29 @@
+import psycopg2 as pg
+class DBInterface(object):
+    def __init__(self, local_mode=False):
+        connection = None
+        try:
+            if local_mode:
+                connection = pg.connect(database="zebras",
+                                        host="localhost",)
+            else:
+                connection = pg.connect(database="zebras",
+                                        host="159.203.214.223",
+                                        user="postgres",
+                                        password="d95705cbb278d5f6930afba6e414f735",
+                                        port=23874,
+                                        sslmode="require")
+            print "Connection Established"
+        except Exception as ex:
+            print "exception type %s %s" % (type(ex).__name__, ex.args)
+            print "Connection Not Established"
+        self.cursor = connection.cursor()
+
+    def get_records(self):
+        query_str = 'SELECT * FROM opioid_data'
+        self.cursor.execute(query_str)
+        colnames = [desc[0] for desc in self.cursor.description]
+        records = self.cursor.fetchall()
+        dictionary_records = [dict((x, y) for x, y in zip(colnames, record)) for record in records]
+        return dictionary_records
+
--- flasktest.py
+++ flasktest.py
@@ -1,10 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, jsonify
+import dbinterface as db
+from pprint import pprint
 
+app = Flask(__name__)
+dbinterface = db.DBInterface()
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
+
+@app.route("/records")
+def records():
+    records = dbinterface.get_records()
+    count = len(records)
+    return jsonify(records=records, queryRecordCount=count, totalRecordCount=count)
+
 
 if __name__ == "__main__":
     app.run()
+    # Bootstrap(app)
