4b5f35ea8dc6015014a5dcba2e64db16b0afcb50
Vincent Van Steenbergen <v.vansteenbergen@gmail.com>
2016-04-24 17:47:44 +0100

    Update db query

--- dbinterface.py
+++ dbinterface.py
@@ -20,10 +20,9 @@ class DBInterface(object):
         self.cursor = connection.cursor()
 
     def get_records(self):
-        query_str = 'SELECT * FROM opioid_data'
+        query_str = 'SELECT state, county, county_population, rx_per_capita_rank, od_per_capita_rank, od_per_rx_rank FROM opioid_data'
         self.cursor.execute(query_str)
         colnames = [desc[0] for desc in self.cursor.description]
         records = self.cursor.fetchall()
         dictionary_records = [dict((x, y) for x, y in zip(colnames, record)) for record in records]
         return dictionary_records
-
